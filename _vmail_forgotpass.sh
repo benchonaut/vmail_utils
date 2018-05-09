fa87bb128d003bc9434996c33e68dd4441559a30
Benjamin Freitag <benjamin.freitag@kosmoskosmos.de>
2018-05-09 13:41:09 +0200

    2018-05-09-11-41

--- _vmail_forgotpass.sh
+++ _vmail_forgotpass.sh
@@ -2,7 +2,7 @@
 if [ -z "$1" ];then echo "NEED DOMAIN AS FIRST ARGUMENT" ; exit 3 ; fi
 if [ -z "$2" ];then echo "NEED USERNAME AS SECOND ARGUMENT"; exit 3 ;fi
 domain=$1;usertogen=$2; 
-$vmailpass=$(grep password /etc/postfix/virtual/mysql-domains.cf|cut -d"=" -f2-|cut -d" " -f2-);
+vmailpass=$(grep password /etc/postfix/virtual/mysql-domains.cf|cut -d"=" -f2-|cut -d" " -f2-);
 #CHECK IF DOMAIN IS REGISTERED IN SYSTEM
 echo "SELECT * FROM vmail.\`domains\` WHERE domain='"$domain"'" | mysql -u vmail -p$vmailpass|grep -q "$domain" || ( echo DOMAIN NOT REGISTERED IN SYSTEM ; exit 3 ) && 
   (echo "SELECT * FROM vmail.\`users\` WHERE domain='"$domain"' AND username='"$usertogen"'" |  mysql -u vmail -p$vmailpass  | grep "$usertogen"  || echo USER DOES NOT EXIST && ( if [ -z "$3" ];then  echo "ENTER PASS";read newmailuserpass ; else newmailuserpass="$3" ; fi ; dovestring=$((echo "$newmailuserpass";echo "$newmailuserpass")|doveadm pw -s SHA512-CRYPT |cut -d"}" -f2-); echo "UPDATE vmail.\`users\` SET password = '"$dovestring"' WHERE domain='"$domain"' AND username='"$usertogen"' ;" | mysql -u vmail -p$vmailpass  ; echo "SELECT * FROM vmail.\`users\` WHERE domain='"$domain"' AND username='"$usertogen"'" | mysql -u vmail -p$vmailpass))
