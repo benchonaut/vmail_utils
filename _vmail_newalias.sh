0df117977e1f92d6788536d94c8e4ffc10ffc762
Benjamin Freitag <benjamin.freitag@kosmoskosmos.de>
2018-09-12 03:20:26 +0200

    2018-09-12-01.20

---
+++ _vmail_newalias.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+if [ -z "$1" ];then echo "NEED DOMAIN AS FIRST ARGUMENT" ; exit 3 ; fi
+domain=$1;
+vmailpass=$(grep password /etc/postfix/virtual/mysql-domains.cf|cut -d"=" -f2-|cut -d" " -f2-);
+#CHECK IF DOMAIN IS REGISTERED IN SYSTEM
---
+++ _vmail_newdomain.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ -z "$1" ];then echo "NEED DOMAIN AS FIRST ARGUMENT" ; exit 3 ; fi
+domain=$1;
+vmailpass=$(grep password /etc/postfix/virtual/mysql-domains.cf|cut -d"=" -f2-|cut -d" " -f2-);
+#CHECK IF DOMAIN IS REGISTERED IN SYSTEM
+echo "SELECT * FROM vmail.\`domains\` WHERE domain='"$domain"'" | mysql -u vmail -p$vmailpass|grep -q "$domain" || ( echo DOMAIN NOT REGISTERED IN SYSTEM ; echo "INSERT INTO vmail.\`domains\` (\`domain\`) VALUES ('"$domain"');" | mysql -u vmail -p$vmailpass  ; echo "SELECT * FROM vmail.\`domains\` WHERE domain='"$domain"';" | mysql -u vmail -p$vmailpass ; exit 3 ) && echo "DOMAIN ALREADY REGISTERED, NOTHING TO DO"
