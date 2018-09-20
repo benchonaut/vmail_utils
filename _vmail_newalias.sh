2414fc10fcadc56fa0cc6cedd8435e9ea14cce65
Benjamin Freitag <benjamin.freitag@kosmoskosmos.de>
2018-09-20 14:01:55 +0200

    2018-09-20-12.01

--- _vmail_newalias.sh
+++ _vmail_newalias.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 if [ -z "$1" ];then echo "NEED DOMAIN AS FIRST ARGUMENT" ; exit 3 ; fi
-if [ -z "$2" ];then echo "NEED USERNAME AS SECOND ARGUMENT"; exit 3 ;fi
+if [ -z "$2" ];then echo "NEED USERNAME AS SECOND ARGUMENT, use @ for catchall"; exit 3 ;fi
 if [ -z "$3" ];then echo "NEED DESTINATION AS THIRD ARGUMENT"; exit 3 ;fi
 domain=$1;usertogen=$2; destination=$3
+if [ "$usertogen" == "@" ] ; then echo CATCHALL DETECTED;usertogen="";fi
 vmailpass=$(grep password /etc/postfix/virtual/mysql-domains.cf|cut -d"=" -f2-|cut -d" " -f2- |sed 's/ //g'|tr -d '\n');
 #CHECK IF DOMAIN IS REGISTERED IN SYSTEM
 echo "SELECT * FROM vmail.\`domains\` WHERE domain='"$domain"'" | mysql -u vmail -p"$vmailpass"|grep -q "$domain" || ( echo DOMAIN NOT REGISTERED IN SYSTEM ; exit 3 ) && 
